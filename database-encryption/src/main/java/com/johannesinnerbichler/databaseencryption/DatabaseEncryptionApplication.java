1381ce5e2ead876eb65656969bf5d012dc6d0672
Johannes Innerbichler <j.innerbichler@gmail.com>
2018-06-09 23:23:07 +0200

    started db encryption project

---
+++ database-encryption/src/main/java/com/johannesinnerbichler/databaseencryption/DatabaseEncryptionApplication.java
@@ -0,0 +1,12 @@
+package com.johannesinnerbichler.databaseencryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseEncryptionApplication.class, args);
+	}
+}
---
+++ database-encryption/src/test/java/com/johannesinnerbichler/databaseencryption/DatabaseEncryptionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.johannesinnerbichler.databaseencryption;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DatabaseEncryptionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
